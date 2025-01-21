5dde8a0af215b2f16e2ee713394345c7418fbaf9
Levi <levi.tobio2505@gmail.com>
2025-01-20 21:59:01 -0300

    :building_construction: structure: estrutura do projeto - criando os componentes, containers e páginas

---
+++ src/containers/carousel/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles.ts"
+
+const Carousel = () =>{
+    return(
+        <S.Header>asdasdsda</S.Header>
+    )
+}
+
+export default Carousel
---
+++ src/pages/home/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Carousel from "../../containers/carousel/main";
+
+const Home = () =>{
+    return(
+        <Carousel>
+        </Carousel>
+    )
+}
+
+export default Home
